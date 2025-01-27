06d8abb82689880a2d0b359a85c47557289cc3ec
Joan <j.ricartt@gmail.com>
2025-01-27 13:25:57 +0100

    swap theme to everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,14 @@
 local M = {
-	"ellisonleao/gruvbox.nvim",
+	"sainnhe/everforest",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("gruvbox")
+		-- Optionally configure and load the colorscheme
+		-- directly inside the plugin declaration.
+		vim.g.everforest_enable_italic = true
+		vim.g.everforest_background = 'hard'
+		vim.g.everforest_float_style = 'dim'
+		vim.cmd.colorscheme('everforest')
 	end
 }
 
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		opts = {
-			theme = 'gruvbox'
+			theme = 'everforest'
 		}
 	}
 }
