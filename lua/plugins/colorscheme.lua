d984b83e3ad8f9a10c6f1efa8f43b3a0fd7fd944
Joan <j.ricartt@gmail.com>
2025-01-31 08:36:01 +0100

    changed theme to ash

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,9 @@
 local M = {
-	"sainnhe/everforest",
+	"drewxs/ash.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- Optionally configure and load the colorscheme
-		-- directly inside the plugin declaration.
-		vim.g.everforest_enable_italic = true
-		vim.g.everforest_background = 'hard'
-		vim.g.everforest_float_style = 'dim'
-		vim.cmd.colorscheme('everforest')
+		vim.cmd.colorscheme('ash')
 	end
 }
 
